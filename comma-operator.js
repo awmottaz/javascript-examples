6fef64c4400e34fe790d261b552b572d886472ff
Anthony Mottaz <anthonywmottaz@gmail.com>
2017-03-29 10:10:52 -0500

    Update comma-operator.js

--- comma-operator.js
+++ comma-operator.js
@@ -1,7 +1,5 @@
 /* The comma operator evaluates a sequence of expressions and returns the result of the last one only */
-
-var a = b = 4, c = 2
+var a = b = 4, c = 2 // 2 is returned to the console
+var x = (y = 7, z = 5) // 5 is returned in the parenthetical expression, and so is assigned to x
 console.log(a) // => 4
-
-var x = (y = 7, z = 5)
 console.log(x) // => 5
