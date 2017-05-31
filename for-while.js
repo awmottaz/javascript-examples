3b0f24aa1997fc7f9c7db33eb77819bc3e42096a
Anthony Mottaz <anthonywmottaz@gmail.com>
2017-05-31 14:56:37 -0500

    Update for-while.js

--- for-while.js
+++ for-while.js
@@ -7,16 +7,16 @@ for ([initialization]; [condition]; [final-expression]) {
   statement
 }
 
-The initialization step is run before looping begins. Usually
+The [initialization] step is run before looping begins. Usually
 this is used just to declare the variable that will be used for
 looping, but you can evaluate any expression in there.
 
-The condition step determines whether the statement block will be
+The [condition] step determines whether the statement block will be
 run. Although this is usually used to check the iterator against
 some condition, we can put any conditional statement here, no matter
 how complex.
 
-The final expression, like the initialization, can contain any
+The [final-expression], like the initialization, can contain any
 JavaScript expression we want. This is evaluated at the conclusion
 of every loop.
 
@@ -38,7 +38,7 @@ for (let i = 1; i < 10; i++) {
 I prefer to avoid using the `break` statement. In my own
 taste, it is a rather ugly control statement.
 
-Luckily, we can include this logical check in the condition
+Luckily, we can include this logical check in the [condition]
 portion of our for loop!
 */
 
@@ -53,7 +53,7 @@ This version makes it more clear what the conditions are for
 continuing a loop, and it avoids the ugly `break`.
 
 Let's take it a step further... Remember how I said any expression
-can go in the initialization? The same code can be rewritten as
+can go in the [initialization]? The same code can be rewritten as
 this:
 */
 
@@ -63,14 +63,14 @@ for (let i = 0, cond = true; i < 10 && cond; i++) {
 }
 
 /*
-We just used the initialization step to initialize both the counter
+We just used the [initialization] step to initialize both the counter
 and the boolean. This is nice because we are moving towards a
 complete separation of the looping control logic from the statements
 executed within the loop.
 
 Yet we still have that pesky `cond` assignment within the statement block.
-But we can move this to the final expression! Be careful with counting, though.
-In our example so far, the output of the program would be
+But we can move this to the [final-expression]! Be careful with the order of
+execution—In our example so far, the output of the program would be
 
 0
 1
@@ -79,7 +79,7 @@ In our example so far, the output of the program would be
 4
 5
 
-This is because `i` is incremented in the final expression after `cond = i < 5`
+This is because `i` is incremented in the [final-expression] after `cond = i < 5`
 has been evaluated. Our final program is this:
 */
 
