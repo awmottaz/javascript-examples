3bb6e750e1c2fafdf7529ae8edb7cf9315690756
Anthony Mottaz <anthonywmottaz@gmail.com>
2018-12-21 16:45:06 -0600

    Update non-negative-test.js

--- non-negative-test.js
+++ non-negative-test.js
@@ -1,31 +1,34 @@
-/*
-This function accepts any input and returns true only if the input
-can be parsed as a non-negative integer.
-
-The first line casts the argument into an integer (or NaN) and then
-uses Math.abs to make it non-negative.
-
-The second line casts the argument into a number, possibly decimal (or NaN).
-
-If the argument was a non-negative integer, these two values should agree.
-
-Since JavaScript evaluates 'NaN === NaN' to false, this function is robust to
-bad input.
-*/
-
-function isNonNegativeInt (arg) {
+/**
+ * This function accepts any input and returns true only if the input
+ * can be parsed as a non-negative integer.
+ *
+ * The first line casts the argument into an integer (or NaN) and then
+ * uses Math.abs to make it non-negative.
+ *
+ * The second line casts the argument into a number, possibly decimal (or NaN).
+ *
+ * If the argument was a non-negative integer, these two values should agree.
+ *
+ * Since JavaScript evaluates 'NaN === NaN' to false, this function is robust to
+ * bad input.
+ * @param {*} arg
+ * @returns {boolean} true if arg is parse-able a non-negative integer, false otherwise.
+ */
+function isNonNegativeInt(arg) {
   var nonNegInt = Math.abs(parseInt(arg, 10))
   var num = Number(arg)
-  
+
   return num === nonNegInt
 }
 
-/*
-By skipping the variable assignment and using ES6 syntax, we can greatly
-reduce this function's footprint:
-*/
+// By skipping the variable assignment and using ES6 syntax, we can greatly
+// reduce this function's footprint:
 
-const nni = arg => (Math.abs(parseInt(arg, 10)) === Number(arg))
+/**
+ * @param {*} arg
+ * @returns {boolean} true if arg is parse-able a non-negative integer, false otherwise.
+ */
+const nni = arg => Math.abs(parseInt(arg, 10)) === Number(arg)
 
 // Each of these logs "true"
 console.log(nni(1))
@@ -36,7 +39,11 @@ console.log(!nni(3.14))
 console.log(!nni(true))
 console.log(!nni())
 console.log(!nni(null))
-console.log(!nni(() => { return 42 }))
+console.log(
+  !nni(() => {
+    return 42
+  })
+)
 console.log(!nni(NaN))
 console.log(!nni([1, 2, 3]))
 console.log(!nni({ foo: 'bar' }))
